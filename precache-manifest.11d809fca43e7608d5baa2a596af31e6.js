fd3b8cd313ccd7a3198a22700a4ac129c02e89c5
williamj-in <williamjinq@outlook.com>
2020-05-01 13:52:24 -0400

    Updates

---
+++ precache-manifest.11d809fca43e7608d5baa2a596af31e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae87549e7022516bda60d8c62e25503d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d0a77543f56a5a651cab",
+    "url": "./static/css/main.2bbab154.chunk.css"
+  },
+  {
+    "revision": "5c7dd8d2926e9787abb6",
+    "url": "./static/js/2.be79f379.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "./static/js/2.be79f379.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0a77543f56a5a651cab",
+    "url": "./static/js/main.53c0844d.chunk.js"
+  },
+  {
+    "revision": "06f8d410bdc7529f8e63",
+    "url": "./static/js/runtime-main.4bcc66e8.js"
+  }
+]);
